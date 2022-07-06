994d41679a4553777652669ce2ed4a7a54c5e12c
ImAmlan03 <imamlan03@gmail.com>
2022-07-06 18:03:11 +0530

    Replaced some input fields with drop down menus, some dummy php to test POST

--- connect.php
+++ connect.php
@@ -1,4 +1,10 @@
 <?php
-    $dept_name = $_POST["dept_name"];
-    echo $dept_name;
+   $s_name = $_POST["s_name"];
+   $department = $_POST["department"];
+   $admission_yr = $_POST["admission_yr"];
+   $s_gender = $_POST["s_gender"];
+   echo "<h4><strong>Student Name: </strong>", $s_name, "</h4><br>";
+   echo "<h4><strong>Student Department: </strong>", $department, "</h4><br>";
+   echo "<h4><strong>Admission Year: </strong>", $admission_yr, "</h4><br>";
+   echo "<h4><strong>Gender: </strong>", $s_gender, "</h4><br>";   
 ?>
